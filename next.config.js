3c92428ae958e605aa3f0532ca11b8741751de32
antoniopataro <antonio.pataro12@gmail.com>
2022-06-20 19:25:05 -0300

    fix: basepath '/'

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/e-commerce/',
+  basePath: '/e-commerce',
   assetPrefix: '/e-commerce/',
   images: {
     loader: 'akamai',
