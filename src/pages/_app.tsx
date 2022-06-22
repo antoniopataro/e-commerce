ae048a98dfcdfd5fab40d65cbb06768fb58a7c72
antoniopataro <antonio.pataro12@gmail.com>
2022-06-22 11:01:35 -0300

    style: imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 
-import { Provider } from 'react-redux';
 import store from '../redux/store';
+import { Provider } from 'react-redux';
 
 import Layout from '../components/Layout';
 
